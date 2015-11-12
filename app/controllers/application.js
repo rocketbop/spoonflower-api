f0adb9e30008ffe9ecf8449c568337e03ffb05d3
paulgbyrne <paulgregorybyrne@gmail.com>
2015-11-12 15:58:13 +0100

    Remove unused code

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -5,9 +5,4 @@ export default Ember.Controller.extend({
   queryParams: [
     'state'
   ],
-  filteredProducts: Ember.computed('model.products@each.name', 'model.filter', function () {
-    var products = this.get('model.products');
-    var filter = this.get('model.filter');
-    return products.filterBy('productCategoryId', filter);
-  })
 });
